5bcf32e743e7fa3cf10e240b92c4cf1bdb1c76e4
Mariia <marii.ch@bk.ru>
2022-10-04 11:23:16 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 3
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число c");
+int c = int.Parse(Console.ReadLine());
+
+if (a < b && b > c)
+        {
+            Console.WriteLine("макс = " + b);
+        }
+
+    else if (b < a && a > c)
+        {
+            Console.WriteLine("макс = " + a);
+        }
+
+    else if (a < c && c > b)
+        {
+            Console.WriteLine("макс = " + c);
+        }
+
